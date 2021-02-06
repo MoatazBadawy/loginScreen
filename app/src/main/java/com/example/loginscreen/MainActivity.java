0f205ded159f7a4c79695d6b79897ef927c727f1
MoatazBadawy <moataz.egy101@gmail.com>
2021-02-06 23:35:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //make the status bar white with black icons
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+}
